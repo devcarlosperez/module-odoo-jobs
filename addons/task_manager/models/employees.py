548d0c1223c70020c9124f9c3486c2f4c2ce357c
devcarlosperez <carlosperezsantana04@gmail.com>
2025-05-19 09:34:20 +0100

    connected modules and bug fixes

--- addons/task_manager/models/employees.py
+++ addons/task_manager/models/employees.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 class Employee(models.Model):
@@ -12,10 +10,14 @@ class Employee(models.Model):
     birth_date = fields.Date()
     hire_date = fields.Date()
     country = fields.Many2one("res.country", "Country")
-    task_ids = fields.Many2many('task_manager.task', 'Tasks')
-
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    task_ids = fields.Many2many(
+    'task_manager.task',
+    'employee_task_rel',
+    'employee_id',
+    'task_id',
+    string="Tasks"
+)
+    job_position_id = fields.Many2one(
+        'jobs.job_position',
+        string="Job Position"
+    )
--- addons/task_manager/models/tasks.py
+++ addons/task_manager/models/tasks.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 class Task(models.Model):
@@ -15,9 +13,10 @@ class Task(models.Model):
         ('1', 'Media'),
         ('2', 'Alta')])
     deadline = fields.Date()
-    employee_ids = fields.Many2many('task_manager.employee', 'Employees')
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    employee_ids = fields.Many2many(
+    'task_manager.employee',
+    'employee_task_rel',
+    'task_id',
+    'employee_id',
+    string="Employees"
+)
